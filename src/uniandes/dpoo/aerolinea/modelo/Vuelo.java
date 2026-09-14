caeb59f9aefc476265cfe5ab42fb844cd1894166
Iván Vargas Reyes <i.vargasr2@uniandes.edu.co>
2026-09-14 10:48:24 -0500

    vuelo

--- src/uniandes/dpoo/aerolinea/modelo/Vuelo.java
+++ src/uniandes/dpoo/aerolinea/modelo/Vuelo.java
@@ -1,12 +1,20 @@
 package uniandes.dpoo.aerolinea.modelo;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Vuelo {
 	private String fecha;
 	private Ruta ruta;
 	private Avion avion;
+	private Map<String, Tiquete> mapaTiquetes;
 	
 	public Vuelo(Ruta ruta, String fecha, Avion avion) {
-		
+		this.fecha = fecha;
+		this.ruta = ruta;
+		this.avion = avion;
+		this.mapaTiquetes = new HashMap<>();
 	}
 
 }
