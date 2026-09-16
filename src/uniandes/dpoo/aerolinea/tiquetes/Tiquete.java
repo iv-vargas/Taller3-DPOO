587489a8af0111fffaf61adc4827a23e0dea144a
Iván Vargas Reyes <i.vargasr2@uniandes.edu.co>
2026-09-15 20:30:09 -0500

    tiquetes

--- src/uniandes/dpoo/aerolinea/tiquetes/Tiquete.java
+++ src/uniandes/dpoo/aerolinea/tiquetes/Tiquete.java
@@ -1,5 +1,46 @@
 package uniandes.dpoo.aerolinea.tiquetes;
 
-public class Tiquete {
+import uniandes.dpoo.aerolinea.modelo.Vuelo;
 
+public class Tiquete {
+	
+	private String codigo;
+	private int tarifa;
+	private boolean usado;
+	private Vuelo vueloAsociado;
+	private Cliente cliente;
+	
+	
+	public Tiquete(String codigo, Vuelo vuelo, Cliente clienteComprador, int tarifa) {
+		this.codigo = codigo;
+		this.vueloAsociado = vuelo;
+		this.cliente = clienteComprador;
+		this.tarifa = tarifa;
+		this.usado = false;
+		
+	}
+	
+	public String getCodigo() {
+		return codigo;
+	}
+	public int getTarifa() {
+		return tarifa;
+	}
+	public boolean esUsado() {
+		return usado;
+	}
+	public Vuelo getVueloAsociado() {
+		return vueloAsociado;
+	}
+	public Cliente getCliente() {
+		return cliente;
+	}
+	public void marcarComoUsado() {
+		this.usado = true;
+	}
+	
+	
+	
+
+	
 }
